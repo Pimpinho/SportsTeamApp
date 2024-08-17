4ce7e73956b928a7a4023f565a76056002efc256
Abraão Melo <amsd@ic.ufal.br>
2024-08-16 23:40:58 -0300

    add templatesView and urls

--- SportTeamApp/urls.py
+++ SportTeamApp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import home, contact
+from .views import FinancialView, HomeView, InventoryView, PlayerView, ScheduleView, StatsView, TeamView 
 
 urlpatterns = [
-    path('', home),
-    path('contact', contact),
+    path('', HomeView.as_view(), name='home'),
+    path('financial/', FinancialView.as_view(), name='financial'),
+    path('inventory/', InventoryView.as_view(), name='inventory'),
+    path('player/', PlayerView.as_view(), name='player'),
+    path('schedule/', ScheduleView.as_view(), name='schedule'),
+    path('stats/', StatsView.as_view(), name='stats'),
+    path('team/', TeamView.as_view(), name='team'),
 ]
